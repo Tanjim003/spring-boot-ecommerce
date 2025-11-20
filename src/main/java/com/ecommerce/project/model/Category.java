a0844da4115751e3be064962298a99a3a4d059c8
Tanjim003 <tanjim.hossain24@yahoo.com>
2025-11-21 02:57:09 +0600

    Add a custom exception handler then used @size annotations to make validations mroe easier

--- src/main/java/com/ecommerce/project/model/Category.java
+++ src/main/java/com/ecommerce/project/model/Category.java
@@ -4,7 +4,9 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -17,7 +19,8 @@ public class Category {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long categoryId;
-    @NotNull
+    @NotBlank(message ="category can't be blank !")
+    @Size(min = 5, message="Category name must contain at least 5 character ): ")
     private String categoryName;
     
 }
