6bc1ce189a27104d5cce1280df3aaaa9f211422f
Tanjim003 <tanjim.hossain24@yahoo.com>
2025-11-20 09:24:06 +0600

    introduce lombok jpa-validation to project

--- src/main/java/com/ecommerce/project/controller/CategoryController.java
+++ src/main/java/com/ecommerce/project/controller/CategoryController.java
@@ -2,6 +2,7 @@ package com.ecommerce.project.controller;
 
 import com.ecommerce.project.model.Category;
 import com.ecommerce.project.service.CategoryService;
+import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -27,7 +28,7 @@ public class CategoryController {
     }
     @PostMapping("/api/public/categories")
     //@RequestMapping(value="/api/public/categories", method=RequestMethod.POST)
-    public ResponseEntity<String> createCategory(@RequestBody Category category){
+    public ResponseEntity<String> createCategory(@Valid @RequestBody Category category){
 
         categoryService.createCategory(category);
         return new ResponseEntity<>( "Category added succesfully", HttpStatus.CREATED);
@@ -41,11 +42,11 @@ public class CategoryController {
             return new ResponseEntity<>(e.getReason(), e.getStatusCode());
         }
     }
-    @PutMapping("api/public/categoryId")
+    @PutMapping("api/public/categories/{categoryId}")
     public ResponseEntity<String> updateCategory(@RequestBody Category category , @PathVariable Long categoryId){
         try {
             Category savedCategory  = categoryService.updateCategory(category,categoryId);
-            return new ResponseEntity<>("Category with category id: " + category,  HttpStatus.OK);
+            return new ResponseEntity<>("Category with category id: " + categoryId + "updated successfully !",  HttpStatus.OK);
         }
         catch (ResponseStatusException e){
             return new ResponseEntity<>(e.getReason(),e.getStatusCode());
