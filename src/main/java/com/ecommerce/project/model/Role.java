566bc900ff44c09529088d7914cd466332b9e626
Tanjim003 <tanjim.hossain24@yahoo.com>
2026-02-27 03:21:39 +0600

    started add jwt in the application with custom userDetails

--- src/main/java/com/ecommerce/project/model/Role.java
+++ src/main/java/com/ecommerce/project/model/Role.java
@@ -1,11 +1,13 @@
 package com.ecommerce.project.model;
 
 import jakarta.persistence.*;
+import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 
 @Entity
 @NoArgsConstructor
+@Data
 @Table(name="roles")
 public class Role {
     @Id
